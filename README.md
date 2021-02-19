5a02fc6ab415977b620b5cda5efd6207ade776c9
Monun <monun1010@gmail.com>
2021-02-19 19:02:40 +0900

    릴리스

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Fortress
-[![Build Status](https://travis-ci.com/monun/fortress.svg?branch=master)](https://travis-ci.org/monun/fortress)
+[![Build Status](https://travis-ci.com/monun/minigame-fortress.svg?branch=master)](https://travis-ci.org/monun/minigame-fortress)
+
+[![](http://img.youtube.com/vi/MVLbOHXiem0/0.jpg)](http://www.youtube.com/watch?v=MVLbOHXiem0 "")
+
+* 1.16.5
+* JDK 11
+* [서버](https://github.com/monun/fortress/releases/download/0.0.1/server.ps1)
--- server.ps1
+++ server.ps1
@@ -1,3 +1,6 @@
+# Powershell
+$TitlePrefix = "Paper"
+
 # Gradle
 $Build = $true
 
@@ -5,19 +8,26 @@ $Build = $true
 $Xms = "1G"
 $Xmx = "2G"
 $Debug = $true
-$Jar = "paper.jar"
+$ServerJar = "paper.jar"
 
-# Spigot
+# Server
 $Version = "1.16.5"
 
 # Plugins
-$Plugins = (
-    "https://github.com/monun/kotlin-plugin/releases/download/1.4.30/Kotlin.jar",
-    "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/target/ProtocolLib.jar"
+$Plugins = @(
+"https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.4.30.jar",
+"https://github.com/monun/team-spawn/releases/latest/download/TeamSpawn.jar",
+"https://github.com/monun/block-trigger/releases/latest/download/BlockTrigger.jar",
+"https://github.com/monun/delayed-respawn/releases/latest/download/DelayedRespawn.jar",
+"https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/target/ProtocolLib.jar",
+"https://media.forgecdn.net/files/3172/946/worldedit-bukkit-7.2.2-dist.jar"
 )
 
-# Backup
+# Backup (7z)
 $AskBackup = $false
+
+# Run
+$ServerJarURL = "https://papermc.io/api/v1/paper/$Version/latest/download"
 
 Function Download-File
 {
@@ -155,13 +165,16 @@ function Choice
 
 Function Backup
 {
+    param(
+        [string]$ServerJar
+    )
     $Backup = "backup"
 
     Create-Directory $Backup
 
     $Date = Get-Date -Format "yyyy-MM-dd HHmmss"
     $ArchiveName = "$Backup/$Date.zip"
-    7z a -tzip $ArchiveName ./ "-xr!*.gz" "-x!paper.jar" "-x!paper.ps1" "-x!backup" "-x!cache" | Out-Null
+    7z a -tzip $ArchiveName ./ "-xr!*.gz" "-x!$ServerJar" "-x!backup" "-x!cache" | Out-Null
     Write-Host "Backup completed $ArchiveName"
 }
 
@@ -171,11 +184,11 @@ $Folder = ".$( (Get-Item($MyInvocation.MyCommand.Name)).BaseName )"
 # Setup
 Create-Directory $Folder
 Set-Location $Folder
-$host.UI.RawUI.WindowTitle = "paper $Version"
-Agree-EULA
+$host.UI.RawUI.WindowTitle = "$TitlePrefix $Version"
+Agree-EULA # alt -> -Dcom.mojang.eula.agree=true
 
-# Download paper
-Download-File "https://papermc.io/api/v1/paper/$Version/latest/download" "." $Jar
+# Download servver jar
+Download-File $ServerJarURL "." $ServerJar
 
 # Download plugins
 foreach ($Plugin in $Plugins)
@@ -195,7 +208,7 @@ if ($Debug)
 }
 
 [void]$JVMArgs.Add("-jar")
-[void]$JVMArgs.Add($Jar)
+[void]$JVMArgs.Add($ServerJar)
 [void]$JVMArgs.Add("--nogui")
 
 # Run
@@ -204,7 +217,7 @@ While ($true)
     if ($Build)
     {
         Set-Location $ProjectFolder
-        ./gradlew clean paper
+        ./gradlew clean copyToServer
         Set-Location $Folder
     }
 
@@ -216,11 +229,11 @@ While ($true)
 
         if ($SkipBackup -eq 'N')
         {
-            Backup
+            Backup $ServerJar
         }
     }
 
-    $Restart = Choice "Restart? [Y]es [N]o" @('Y', 'N') 'Y' 2
+    $Restart = Choice "Restart? [Y]es [N]o" @('Y', 'N') 'Y' 3
 
     if ($Restart -eq 'N')
     {
